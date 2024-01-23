ab29192b7cc268bfc276a9a5a3996be6af143691
Bakel <r.van.bakel@vu.nl>
2024-01-23 17:24:47 +0100

    Initial code and data commit

---
+++ lod_preprocessor.cpp
@@ -0,0 +1,248 @@
+// #include <cstdint>
+// #include <vector>
+// #include <stack>
+// #include <span>
+#include <fstream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+// #include <boost/dynamic_bitset.hpp>
+#include <boost/unordered/unordered_flat_map.hpp>
+// #include <boost/unordered/unordered_flat_set.hpp>
+// #include <boost/format.hpp>
+#include <iostream>
+// #include <boost/algorithm/string/find.hpp>
+
+using edge_type = uint32_t;
+using node_index = uint64_t;
+
+class MyException : public std::exception
+{
+private:
+    const std::string message;
+
+public:
+    MyException(const std::string &err) : message(err) {}
+
+    const char *what() const noexcept override
+    {
+        return message.c_str();
+    }
+};
+
+template <typename T>
+class IDMapper
+{
+    boost::unordered_flat_map<std::string, T> mapping;
+
+public:
+    IDMapper() : mapping(100000000)
+    {
+    }
+
+    T getID(std::string &stringID)
+    {
+        T potentially_new = mapping.size();
+        // std::pair<boost::unordered_flat_map<std::string, T>::const_iterator, bool>
+        auto result = mapping.try_emplace(stringID, potentially_new);
+        T the_id = (*result.first).second;
+        return the_id;
+    }
+
+    // template <class Stream>
+    void dump(std::ostream &out)
+    {
+        for (auto a = this->mapping.cbegin(); a != this->mapping.cend(); a++)
+        {
+            std::string str(a->first);
+            T id = a->second;
+            out << str << " " << id << '\n';
+        }
+        out.flush();
+    }
+
+    void dump_to_file(const std::string &filename)
+    {
+        std::ofstream mapping_out(filename, std::ios::trunc);
+        if (!mapping_out.is_open())
+        {
+            throw MyException("Opening the file to dump to failed");
+        }
+        this->dump(mapping_out);
+        mapping_out.close();
+    }
+};
+
+u_int64_t read_uint64_little_endian(std::istream &inputstream){
+    char data[8];
+    inputstream.read(data, 8);
+    u_int64_t result = uint64_t(0) ;
+
+    for (unsigned int i = 0; i < 8; i++){
+        result |= uint64_t(data[i]) << (i*8);
+    }
+    return result;
+}
+
+void write_uint64_little_endian(std::ostream &outputstream, u_int64_t value){
+    char data[8];
+    for (unsigned int i = 0; i < 8; i++){
+        data[i] = char(value & 0xFFFFFFFFFFFFFF00ull);
+        value = value >> 8;
+    }
+    outputstream.write(data, 8);
+}
+
+u_int32_t read_uint32_little_endian(std::istream &inputstream){
+    char data[4];
+    inputstream.read(data, 4);
+    u_int32_t result = uint32_t(0) ;
+
+    for (unsigned int i = 0; i < 4; i++){
+        result |= uint32_t(data[i]) << (i*4);
+    }
+    return result;
+}
+
+void write_uint32_little_endian(std::ostream &outputstream, u_int32_t value){
+    char data[4];
+    for (unsigned int i = 0; i < 4; i++){
+        data[i] = char(value & 0xFFFFFFFFFFFFFF00ull);
+        value = value >> 4;
+    }
+    outputstream.write(data, 4);
+}
+
+void convert_graph(std::istream &inputstream,
+                            std::ostream &outputstream, 
+                            const std::string &node_ID_file,
+                            const std::string &edge_ID_file)
+{
+    IDMapper<node_index> node_ID_Mapper;
+    IDMapper<edge_type> edge_ID_Mapper;
+
+    const int BufferSize = 8 * 16184;
+
+    char _buffer[BufferSize];
+
+    inputstream.rdbuf()->pubsetbuf(_buffer, BufferSize);
+
+    std::string line;
+    unsigned long line_counter = 0;
+
+    std::getline(inputstream, line);
+    boost::trim(line);
+    if  (line != "<https://krr.triply.cc/krr/lod-a-lot/graphs/default> {")
+    {
+        throw MyException("This binary is specific for the full bisimulation of krr.triply.cc/krr/lod-a-lot/");
+    }
+
+    bool must_end = false;
+
+    while (std::getline(inputstream, line))
+    {
+        const std::string original_line(line);
+        line_counter++;
+
+        boost::trim(line);
+        if (line[0] == '#' || line == "") 
+        {
+            // ignore comment line
+            continue;
+        }
+        if (must_end)
+        {
+            throw MyException("The file must have ended here, but did not!");
+        }
+        if (line == "}")
+        {
+            must_end = true;
+            continue;
+        }
+        if (!(*(line.cend() - 1) == '.'))
+        {
+            throw MyException("The line '" + original_line + "' did not end in a period(.)");
+        }
+        if (!(*(line.cend() - 2) == ' '))
+        {
+            throw MyException("The line '" + original_line + "' did not end in a space before the period(.)");
+        }
+        line = line.substr(0, line.length() - 2);
+        boost::trim(line);
+
+
+        // subject
+        // split in 2 pieces
+        size_t deliminer_start1 = line.find("> <");
+        std::string subject = line.substr(0,deliminer_start1);
+        std::string predicate_object = line.substr(deliminer_start1+3,line.size());
+
+        if (subject[0] != '<')
+        {
+            throw MyException("The subject '" + subject + "' did not start with a '<'");
+        }
+        subject = subject.substr(1, subject.size());
+
+        // predicate
+        // split in 2 pieces
+
+        // std::vector<std::string> parts2;
+        // boost::iter_split(parts2, predicate_object, boost::first_finder("> "));
+        size_t deliminer_start2 = predicate_object.find("> ");
+        std::string predicate = predicate_object.substr(0, deliminer_start2);
+        std::string object_literal_or_entity = predicate_object.substr(deliminer_start2+2, predicate_object.size());
+
+        if (predicate[0] == '<')
+        {
+            throw MyException("The predicate '" + predicate + "' did start with a double '<'");
+        }
+
+        // object
+        // final part is the object
+        boost::trim(object_literal_or_entity);
+
+
+        std::string object;
+        if (object_literal_or_entity[0] == '"'){
+            // it is a literal
+            object = "bisimulation:string";
+        } else if (object_literal_or_entity[0] == '<') {
+            // it is an entity
+            object = object_literal_or_entity.substr(1, object_literal_or_entity.size() - 1);
+        } else {
+            throw MyException("The object '" + object_literal_or_entity + "' did not start with \" or <");
+        }
+
+        // subject
+        node_index subject_index = node_ID_Mapper.getID(subject);
+        // object
+        node_index object_index = node_ID_Mapper.getID(object);
+        // edge
+        edge_type edge_index = edge_ID_Mapper.getID(predicate);
+
+        // output the line
+        // This should be binary writing instead.
+        write_uint64_little_endian(outputstream, subject_index);
+        write_uint32_little_endian(outputstream, edge_index);
+        write_uint64_little_endian(outputstream, object_index);
+
+        if (line_counter % 1000000 == 0)
+        {
+            std::cout << "done with " << line_counter << " triples" << std::endl;
+        }
+    }
+
+    node_ID_Mapper.dump_to_file(node_ID_file);
+    edge_ID_Mapper.dump_to_file(edge_ID_file);
+}
+
+
+int main(int ac, char *av[])
+{
+    std::ifstream infile("./Laurence_Fishburne_Custom_Shuffled.trig", std::ifstream::in);
+    std::ofstream output_file("./output_filename.txt", std::ifstream::out);
+    std::string node_ID_file = "./entity2ID.txt";
+    std::string rel_ID_file = "./rel2ID.txt";
+    
+    convert_graph(infile, output_file, node_ID_file, rel_ID_file);
+    output_file.flush();
+}
