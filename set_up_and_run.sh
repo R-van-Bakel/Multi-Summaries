d49170193473e20881c2d9358ddaae089fbddbec
Bakel <r.van.bakel@vu.nl>
2024-02-01 09:44:01 +0100

    Updated the compilation commands

--- set_up_and_run.sh
+++ set_up_and_run.sh
@@ -3,7 +3,7 @@ mkdir ./bin/
 mkdir ./output/
 BOOST_HOME=/usr/include/boost_1_84_0/
 set -x
-gcc -I $BOOST_HOME ./code/lod_preprocessor.cpp -o ./bin/lod_preprocessor -lstdc++ -Ofast
-gcc -I /usr/include/boost_1_84_0/ ./code/lod_binary_reader.cpp  -o ./bin/lod_binary_reader -lstdc++ -Ofast
+g++ -static -std=c++20 -Wall -Wpedantic -Ofast -fdiagnostics-color=always -I $BOOST_HOME ./code/lod_preprocessor.cpp -o ./bin/lod_preprocessor
+g++ -static -std=c++20 -Wall -Wpedantic -Ofast -fdiagnostics-color=always -I $BOOST_HOME ./code/lod_binary_reader.cpp -o ./bin/lod_binary_reader
 ./bin/lod_preprocessor
 ./bin/lod_binary_reader
